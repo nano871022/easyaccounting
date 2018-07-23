8620d05c68736be1f2b98b98d01de5f698c8d9af
alejo <alejo@alejo>
2018-07-22 19:11:47 -0500

    dev Se agregro el log de informacion al sistema

--- pyt-contabilidad/pyt-common/src/main/java/org/pyt/common/binario/ABin.java
+++ pyt-contabilidad/pyt-common/src/main/java/org/pyt/common/binario/ABin.java
@@ -1,6 +1,15 @@
 package org.pyt.common.binario;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.commons.lang3.StringUtils;
 import org.pyt.common.common.ADto;
+import org.pyt.common.common.Log;
+
 
 /**
  * Clase abstracta que se encarga de tener funcionalidades para usar el entre
@@ -11,6 +20,8 @@ import org.pyt.common.common.ADto;
  */
 public abstract class ABin {
 	private String path_split = "/";
+	private final static String file_properties = "properties/data.properties";
+	private final static String default_file_data = "./systemFile";
 	public ABin() {
 		path_split = System.getProperty("file.separator");
 	}
@@ -38,8 +49,32 @@ public abstract class ABin {
 	 */
 	protected final String getPath() {
 		String path = "";
-		path = "./systemFile";
+		path = pathData();
 		return path;
+	}
+	
+	private final String pathData() {
+		File properties = new File(file_properties);
+		if(properties.exists()) {
+			try {
+				Properties prop = new Properties();
+				FileInputStream fis = new FileInputStream(properties);
+				if(fis != null) {
+					prop.load(fis);
+					String path = prop.getProperty("path");
+					if(StringUtils.isNotBlank(path)) {
+						return path;
+					}
+				}
+			} catch (FileNotFoundException e) {
+				Log.logger(e);
+			} catch (IOException e) {
+				Log.logger(e);
+			}
+		}else {
+			Log.logger("No existe archivo de propiedades para datos.");
+		}
+		return default_file_data;
 	}
 
 }
