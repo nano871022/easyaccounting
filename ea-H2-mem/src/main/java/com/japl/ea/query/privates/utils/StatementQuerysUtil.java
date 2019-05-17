bc3820e77a202ce9ebe51f0a888fcfb73138e15a
joseAparra <jose.parra@ingeneo.com.co>
2019-05-17 17:03:09 -0500

    DBH2 Se agrega nuevo modulo que contendra la creacion de las tablas y cosas que se deberan cargar en la apicacion cuando es nueva.

--- ea-H2-mem/src/main/java/com/japl/ea/query/privates/utils/StatementQuerysUtil.java
+++ ea-H2-mem/src/main/java/com/japl/ea/query/privates/utils/StatementQuerysUtil.java
@@ -11,70 +11,81 @@ import java.util.Random;
 import org.pyt.common.abstracts.ADto;
 import org.pyt.common.exceptions.ReflectionException;
 
+import com.japl.ea.query.privates.Constants.QueryConstants;
 import com.pyt.query.interfaces.IAdvanceQuerySvc.triggerAction;
 import com.pyt.query.interfaces.IAdvanceQuerySvc.triggerOption;
 
 public class StatementQuerysUtil {
+	private final static String CONST_DTO = "DTO";
+	private final static String CONST_ABC_CHAIN = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+	private final static Integer CONST_MAX_LENGTH = 15;
+	private final static String CONST_PREFIX_TABLE = "MEM_";
+
 	public final <T extends ADto> String fieldToWhere(T obj, boolean valuesInsert) throws ReflectionException {
 		List<String> names = obj.getNameFields();
-		String where = "";
+		String where = QueryConstants.CONST_EMPTY;
 		for (String name : names) {
 			var value = obj.get(name);
 			if (value != null && !valuesInsert) {
 				if (where.length() > 0)
-					where += " AND ";
-				where += name + "=" + valueFormat(value);
+					where += QueryConstants.CONST_SPACE + QueryConstants.CONST_AND + QueryConstants.CONST_SPACE;
+				where += name + QueryConstants.CONST_EQUAL + valueFormat(value);
 			} else if (valuesInsert) {
 				if (where.length() > 0)
-					where += ", ";
-				where += value != null ? valueFormat(value) : "NULL";
+					where += QueryConstants.CONST_COMMA + QueryConstants.CONST_SPACE;
+				where += value != null ? valueFormat(value) : QueryConstants.CONST_NULL;
 			}
 		}
 		return where;
 	}
-	
+
 	public final <T extends Object> String valueFormat(T value) {
-		if(value == null)return null;
-		if(value.getClass() == String.class) {
-			return "'" + value.toString() + "'";
+		if (value == null)
+			return null;
+		if (value.getClass() == String.class) {
+			return QueryConstants.CONST_QUOTE + value.toString() + QueryConstants.CONST_QUOTE;
 		}
-		if(value.getClass() == Date.class) {
-			var sdf = new SimpleDateFormat("yyyy-MM-dd");
-			return "'" + sdf.format(value) + "'";
+		if (value.getClass() == Date.class) {
+			var sdf = new SimpleDateFormat(QueryConstants.CONST_FORMAT_DATE);
+			return QueryConstants.CONST_QUOTE + sdf.format(value) + QueryConstants.CONST_QUOTE;
 		}
-		if(value.getClass() == Timestamp.class) {
-			var dt = new Date(((Timestamp)value).getTime());
-			var sdf = new SimpleDateFormat("yyyy-MM-dd");
-			return "'" + sdf.format(dt) + "'";
+		if (value.getClass() == Timestamp.class) {
+			var dt = new Date(((Timestamp) value).getTime());
+			var sdf = new SimpleDateFormat(QueryConstants.CONST_FORMAT_DATE);
+			return QueryConstants.CONST_QUOTE + sdf.format(dt) + QueryConstants.CONST_QUOTE;
 		}
 		return value.toString();
 	}
-	
+
 	public final <T extends ADto> String fieldToSelect(T obj) throws ReflectionException {
 		List<String> names = obj.getNameFields();
 		String fields = Arrays.toString(names.toArray(new String[names.size()]));
-		fields = fields.replace("[", "");
-		fields = fields.replace("]", "");
+		fields = fields.replace(QueryConstants.CONST_KEY_OPEN, QueryConstants.CONST_EMPTY);
+		fields = fields.replace(QueryConstants.CONST_KEY_CLOSE, QueryConstants.CONST_EMPTY);
 		return fields;
 	}
-	
+
 	public String typeDataDB(String typeJava) {
-		if (typeJava.toLowerCase().contains("long") || typeJava.toLowerCase().contains("int")
-				|| typeJava.toLowerCase().contains("boolean") || typeJava.toLowerCase().contains("big")) {
-			return "NUMBER";
+		if (typeJava.toLowerCase().contains(QueryConstants.CONST_TYPE_LONG)
+				|| typeJava.toLowerCase().contains(QueryConstants.CONST_TYPE_INT)
+				|| typeJava.toLowerCase().contains(QueryConstants.CONST_TYPE_BOOL)
+				|| typeJava.toLowerCase().contains(QueryConstants.CONST_TYPE_BIG)) {
+			return QueryConstants.CONST_TYPE_NUMBER;
 		}
-		if (typeJava.toLowerCase().contains("string") || typeJava.toLowerCase().contains("char")) {
-			return "VARCHAR2(100)";
+		if (typeJava.toLowerCase().contains(QueryConstants.CONST_TYPE_STRING)
+				|| typeJava.toLowerCase().contains(QueryConstants.CONST_TYPE_CHAR)) {
+			return String.format(QueryConstants.CONST_TYPE_VARCHAR_2, 100);
 		}
-		if (typeJava.toLowerCase().contains("date")) {
-			return "DATETIME";
+		if (typeJava.toLowerCase().contains(QueryConstants.CONST_TYPE_DATE)) {
+			return QueryConstants.CONST_TYPE_DATE_TIME;
 		}
-		return "VARCHAR2(30)";
+		return String.format(QueryConstants.CONST_TYPE_VARCHAR_2, 30);
 	}
-	
+
 	public <T extends ADto> String getTableName(T obj) {
-		return "MEM_" + obj.getClass().getSimpleName().replace("DTO", "");
+		return CONST_PREFIX_TABLE + obj.getClass().getSimpleName().replace(CONST_DTO, QueryConstants.CONST_EMPTY);
 	}
+
 	/**
 	 * Se encarga de generar el consecutivo
 	 * 
@@ -82,13 +93,11 @@ public class StatementQuerysUtil {
 	 * @return
 	 */
 	public final <T extends ADto> String genConsecutivo(Class<T> dto, Integer size) {
-		String cadena = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
-		Integer max = 15;
 		String name = dto.getSimpleName();
 		Integer lenSize = size.toString().length();
-		name = name.replace("DTO", "");
+		name = name.replace(CONST_DTO, QueryConstants.CONST_EMPTY);
 		Integer length = name.length();
-		if (length > max) {
+		if (length > CONST_MAX_LENGTH) {
 			name = name.substring(0, 7);
 			length = name.length();
 		}
@@ -102,19 +111,19 @@ public class StatementQuerysUtil {
 		sb.append(fecha.getMinute());
 		sb.append(fecha.getSecond());
 		Random aleatorio = new Random();
-		for (int i = 0; i < max - length - lenSize; i++) {
-			Double valor = aleatorio.nextDouble() * (cadena.length() - 1 + 0);
-			sb.append(cadena.charAt(valor.intValue()));
+		for (int i = 0; i < CONST_MAX_LENGTH - length - lenSize; i++) {
+			Double valor = aleatorio.nextDouble() * (CONST_ABC_CHAIN.length() - 1 + 0);
+			sb.append(CONST_ABC_CHAIN.charAt(valor.intValue()));
 		}
 		sb.append(size);
 		return sb.toString();
 	}
-	
-	public <T extends ADto> String getNameTriggerPOJO(Class<T> obj,triggerOption to,triggerAction... tas) {
-		var name = "";
+
+	public <T extends ADto> String getNameTriggerPOJO(Class<T> obj, triggerOption to, triggerAction... tas) {
+		var name = QueryConstants.CONST_EMPTY;
 		name += to.toString().subSequence(0, 1);
-		for(triggerAction ta : tas) {
-			name += ta.toString().substring(0,1);
+		for (triggerAction ta : tas) {
+			name += ta.toString().substring(0, 1);
 		}
 		name += obj.getSimpleName();
 		return name;
---
+++ ea-sql/src/main/resources/tablas/banco.sql
@@ -0,0 +1,21 @@
+CREATE TABLE TBL_BANK(
+	 scode    VARCHAR2(30)       NOT NULL
+	,screater VARCHAR2(100)      NULL
+	,sdeleter VARCHAR2(100)      NULL
+	,supdater VARCHAR2(100)      NULL
+	,dcreate  DATETIME           NOT NULL DEFAULT NOW()
+	,ddelete  DATETIME           NULL
+	,dupdate  DATETIME           NULL
+    ,sdescription VARCHAR2(200)  NULL
+    ,sstate   VARCHAR2(30)       NOT NULL
+    ,sname    VARCHAR2(50)       NULL
+    ,naccount NUMBER             NULL
+    ,stypebank VARCHAR2(30)      NOT NULL
+    ,stypeaccount VARCHAR2(30)   NOT NULL
+    ,dopening DATETIME           NOT NULL
+    ,dclosing DATETIME           NULL
+);
+ALTER TABLE TBL_BANK ADD PRIMARY KEY (scode);
+ALTER TABLE TBL_BANK ADD FOREIGN KEY (sstate) REFERENCES TBL_PARAMETER(scode);
+ALTER TABLE TBL_BANK ADD FOREIGN KEY (stypebank) REFERENCES TBL_PARAMETER(scode);
+ALTER TABLE TBL_BANK ADD FOREIGN KEY (stypeaccount) REFERENCES TBL_PARAMETER(scode);
