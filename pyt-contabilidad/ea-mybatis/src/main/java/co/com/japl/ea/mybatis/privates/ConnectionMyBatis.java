d8a31becb6729d3f5a56579136fe8a15dbb078fa
joseAparra <jose.parra@ingeneo.com.co>
2019-06-17 17:02:42 -0500

    DBH2 Se realiza ajuste de la conexion por medio de mybatis

--- pyt-contabilidad/ea-mybatis/src/main/java/co/com/japl/ea/mybatis/privates/ConnectionMyBatis.java
+++ pyt-contabilidad/ea-mybatis/src/main/java/co/com/japl/ea/mybatis/privates/ConnectionMyBatis.java
@@ -5,19 +5,25 @@ import java.io.Reader;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import co.com.japl.ea.mybatis.privates.constants.MyBatisConstants;
 
 public final class ConnectionMyBatis {
 
 	private Reader reader;
-	private static String CONST_SQL_MAP_CONFIG = "SqlMapCongif.xml";
+	private SqlSessionFactory sqlSessionFactory;
 
 	public ConnectionMyBatis() throws IOException {
-		reader = Resources.getResourceAsReader(CONST_SQL_MAP_CONFIG);
+		reader = Resources.getResourceAsReader(MyBatisConstants.CONST_SQL_MAP_CONFIG);
+
 	}
-	
+
 	public SqlSession openConnection() {
-		var sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		if (sqlSessionFactory == null) {
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		}
 		return sqlSessionFactory.openSession();
 	}
 
