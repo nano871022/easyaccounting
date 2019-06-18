2f948fb66014ff7d37e6744ecdc3186ae90bd81c
joseAparra <jose.parra@ingeneo.com.co>
2019-06-18 09:12:02 -0500

    DBH2 Se crea nuevo modulo para agregar gestor de base de datos (gdb) por medio de jdbc personalizado.

---
+++ pyt-contabilidad/ea-gdb/module-info.java
@@ -0,0 +1,11 @@
+
+module com.japl.ea.query {
+ requires org.pyt.common;
+ requires com.pyt.query.interfaces;
+ requires com.japl.ea.sql;
+ requires java.sql;
+ requires org.h2.api;
+ requires com.h2database;
+requires org.pyt.common.exceptions;
+ provides com.pyt.query.interfaces.IQuerySvc with co.com.japl.ea.dgb.impl.QueryGDBSvc;
+}
