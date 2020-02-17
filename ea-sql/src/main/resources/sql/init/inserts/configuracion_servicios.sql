d78df033405b6d0d2b9fe5c51116883a09ce46f6
Alejandro Parra <alejandroparral@gmail.com>
2020-02-16 19:44:03 -0500

    load i18n lack Se realiza carga de registros de campos para el inicio de la configuracion de la aplicacion.

--- ea-sql/src/main/resources/sql/init/inserts/configuracion_servicios.sql
+++ ea-sql/src/main/resources/sql/init/inserts/configuracion_servicios.sql
@@ -1,16 +1,6 @@
-INSERT INTO TBL_CONFIGURATION (scode,screater,sdeleter,supdater,dcreate,ddelete,dupdate,sfile,soutputfile,sdescription,nreport,sconfiguration)
-VALUES ('configuracion20190811000000001','admin',NULL,NULL,'2019-08-11',NULL,NULL,NULL,NULL,'Cargue de parametros.',NULL,'Cargue Parametros');
-INSERT INTO TBL_MARKER (scode,screater,sdeleter,supdater,dcreate,ddelete,dupdate,sconfiguration,smarker,norder,siotype) 
-VALUES ('marcador2019081100000000000001','admin',NULL,NULL,'2019-08-11',NULL,NULL,'configuracion20190811000000001','Nombre',1,'Entrada');
-INSERT INTO TBL_MARKER (scode,screater,sdeleter,supdater,dcreate,ddelete,dupdate,sconfiguration,smarker,norder,siotype) 
-VALUES ('marcador2019081100000000000002','admin',NULL,NULL,'2019-08-11',NULL,NULL,'configuracion20190811000000001','Descripcion',2,'Entrada');
-INSERT INTO TBL_MARKER (scode,screater,sdeleter,supdater,dcreate,ddelete,dupdate,sconfiguration,smarker,norder,siotype) 
-VALUES ('marcador2019081100000000000003','admin',NULL,NULL,'2019-08-11',NULL,NULL,'configuracion20190811000000001','Grupo',3,'Entrada');
-INSERT INTO TBL_MARKER (scode,screater,sdeleter,supdater,dcreate,ddelete,dupdate,sconfiguration,smarker,norder,siotype) 
-VALUES ('marcador2019081100000000000004','admin',NULL,NULL,'2019-08-11',NULL,NULL,'configuracion20190811000000001','Orden',4,'Entrada');
-INSERT INTO TBL_MARKER (scode,screater,sdeleter,supdater,dcreate,ddelete,dupdate,sconfiguration,smarker,norder,siotype) 
-VALUES ('marcador2019081100000000000005','admin',NULL,NULL,'2019-08-11',NULL,NULL,'configuracion20190811000000001','Valor',5,'Entrada');
-INSERT INTO TBL_MARKER (scode,screater,sdeleter,supdater,dcreate,ddelete,dupdate,sconfiguration,smarker,norder,siotype) 
-VALUES ('marcador2019081100000000000006','admin',NULL,NULL,'2019-08-11',NULL,NULL,'configuracion20190811000000001','Valor2',6,'Entrada');
-INSERT INTO TBL_MARKER (scode,screater,sdeleter,supdater,dcreate,ddelete,dupdate,sconfiguration,smarker,norder,siotype) 
-VALUES ('marcador2019081100000000000007','admin',NULL,NULL,'2019-08-11',NULL,NULL,'configuracion20190811000000001','Estado',7,'Entrada');
+INSERT INTO "TBL_CONFIGURATION" (SCODE,SCREATER,SDELETER,SUPDATER,DCREATE,DDELETE,DUPDATE,SFILE,SOUTPUTFILE,SDESCRIPTION,NREPORT,SCONFIGURATION) VALUES ('Configuracion201912111630570','admin',null,null,{ts '2019-12-11 00:00:00'},null,null,null,null,'Carga de parametros al sistema de parametros',0,'loadparams');
+INSERT INTO "TBL_CONFIGURATION" (SCODE,SCREATER,SDELETER,SUPDATER,DCREATE,DDELETE,DUPDATE,SFILE,SOUTPUTFILE,SDESCRIPTION,NREPORT,SCONFIGURATION) VALUES ('Configuracion20202161523281','alejo.parra',null,null,{ts '2020-02-16 00:00:00'},null,null,null,null,'Este servicio se encarga de cargar las cuentas contables.',0,'Cuenta Contable');
+INSERT INTO "TBL_CONFIGURATION" (SCODE,SCREATER,SDELETER,SUPDATER,DCREATE,DDELETE,DUPDATE,SFILE,SOUTPUTFILE,SDESCRIPTION,NREPORT,SCONFIGURATION) VALUES ('Configuracion20202161535422','alejo.parra',null,null,{ts '2020-02-16 00:00:00'},null,null,null,null,'Configuración para carga de productos por medio de un archivo csv separados por ; sin encabezados',0,'Servicio Productos');
+INSERT INTO "TBL_CONFIGURATION" (SCODE,SCREATER,SDELETER,SUPDATER,DCREATE,DDELETE,DUPDATE,SFILE,SOUTPUTFILE,SDESCRIPTION,NREPORT,SCONFIGURATION) VALUES ('Configuracion20202161546203','alejo.parra',null,null,{ts '2020-02-16 00:00:00'},null,null,null,null,'Se carga la información de movimientos realizados sobre un producto, entrada y salida. ',0,'Carga Movimientos de Productos');
+INSERT INTO "TBL_CONFIGURATION" (SCODE,SCREATER,SDELETER,SUPDATER,DCREATE,DDELETE,DUPDATE,SFILE,SOUTPUTFILE,SDESCRIPTION,NREPORT,SCONFIGURATION) VALUES ('Configuracion20202161751364','alejo.parra',null,null,{ts '2020-02-16 00:00:00'},null,null,null,null,'Se creo un servicio para carga del lenguage',0,'Servicio de lenguage');
+INSERT INTO "TBL_CONFIGURATION" (SCODE,SCREATER,SDELETER,SUPDATER,DCREATE,DDELETE,DUPDATE,SFILE,SOUTPUTFILE,SDESCRIPTION,NREPORT,SCONFIGURATION) VALUES ('Configuracion20202161754515','alejo.parra',null,null,{ts '2020-02-16 00:00:00'},null,null,null,null,'Servicio que permite cargar centros de costos por medio de un archivo de csv separado por ;',0,'Servicio de centro de costo');
